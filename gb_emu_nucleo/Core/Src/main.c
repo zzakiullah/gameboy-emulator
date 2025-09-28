ced072be37b25316905aeaa6e75937121cdc295e
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2025-09-28 15:56:40 -0400

    Start TFT screen driver

--- gb_emu_nucleo/Core/Src/main.c
+++ gb_emu_nucleo/Core/Src/main.c
@@ -202,7 +202,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|SCREEN_WR_Pin|SCREEN_CD_Pin|SCREEN_CS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, SCREEN_D0_Pin|SCREEN_D1_Pin|SCREEN_D2_Pin|SCREEN_D3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, SCREEN_D4_Pin|SCREEN_D5_Pin|SCREEN_D6_Pin|SCREEN_D7_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -210,12 +216,32 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : LD2_Pin SCREEN_WR_Pin SCREEN_CD_Pin SCREEN_CS_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin|SCREEN_WR_Pin|SCREEN_CD_Pin|SCREEN_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SCREEN_D0_Pin SCREEN_D1_Pin SCREEN_D2_Pin SCREEN_D3_Pin */
+  GPIO_InitStruct.Pin = SCREEN_D0_Pin|SCREEN_D1_Pin|SCREEN_D2_Pin|SCREEN_D3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SCREEN_D4_Pin SCREEN_D5_Pin SCREEN_D6_Pin SCREEN_D7_Pin */
+  GPIO_InitStruct.Pin = SCREEN_D4_Pin|SCREEN_D5_Pin|SCREEN_D6_Pin|SCREEN_D7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SCREEN_RD_Pin */
+  GPIO_InitStruct.Pin = SCREEN_RD_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(SCREEN_RD_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
